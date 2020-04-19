870988b47900e8ec2330097427170766a7a09543
GonzaDDV <gonzaaddv@gmail.com>
2020-04-19 20:08:04 -0300

    Más ordenado todo

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const urls = ["coronavirus.html", "esi.html", "dengue.html", "cambios.html"];
-const txtTiempos = $(".tiempo-estimado");
-let parrafos = 0;
+const urls = [
+  "./html/coronavirus.html",
+  "./html/esi.html",
+  "./html/dengue.html",
+  "./html/cambios.html",
+]
+const txtTiempos = $(".tiempo-estimado")
+let parrafos = 0
 
 $("document").ready(() => {
   var fecha = new Date(),
@@ -8,88 +13,86 @@ $("document").ready(() => {
     meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     mes = fecha.getMonth(),
     año = fecha.getFullYear(),
-    txtFecha = document.getElementsByClassName("fecha-txt")[0];
+    txtFecha = document.getElementsByClassName("fecha-txt")[0]
 
   txtFecha.textContent =
-    "Noticias del día " + dia + "/" + meses[mes] + "/" + año;
+    "Noticias del día " + dia + "/" + meses[mes] + "/" + año
   getHTML(urls[0], function (response) {
-    parrafos = response.documentElement.getElementsByClassName("txt-principal");
-    funcionTotal(urls[0], 0);
-  });
+    parrafos = response.documentElement.getElementsByClassName("txt-principal")
+    funcionTotal(urls[0], 0)
+  })
   getHTML(urls[1], function (response) {
-    parrafos = response.documentElement.getElementsByClassName("txt-principal");
-    funcionTotal(urls[1], 1);
-  });
+    parrafos = response.documentElement.getElementsByClassName("txt-principal")
+    funcionTotal(urls[1], 1)
+  })
   getHTML(urls[2], function (response) {
-    parrafos = response.documentElement.getElementsByClassName("txt-principal");
-    funcionTotal(urls[2], 2);
-  });
+    parrafos = response.documentElement.getElementsByClassName("txt-principal")
+    funcionTotal(urls[2], 2)
+  })
   getHTML(urls[3], function (response) {
-    parrafos = response.documentElement.getElementsByClassName("txt-principal");
-    funcionTotal(urls[3], 3);
-  });
-});
+    parrafos = response.documentElement.getElementsByClassName("txt-principal")
+    funcionTotal(urls[3], 3)
+  })
+})
 
 // Tiempo estimado de lectura
 function funcionTotal(url, index) {
-  const palabrasPorMinuto = 200;
-  var tiempoEstimado = 0;
-  var palabrasTotales = 0;
+  const palabrasPorMinuto = 200
+  var tiempoEstimado = 0
+  var palabrasTotales = 0
   for (i = 0; i < parrafos.length; i++) {
-    palabrasTotales += parrafos[i].textContent.trim().split(/\s+/).length;
+    palabrasTotales += parrafos[i].textContent.trim().split(/\s+/).length
   }
 
-  var tiempoDecimal = palabrasTotales / palabrasPorMinuto;
-  var tiempo = Math.ceil(tiempoDecimal);
+  var tiempoDecimal = palabrasTotales / palabrasPorMinuto
+  var tiempo = Math.ceil(tiempoDecimal)
   if (tiempo == 0) {
-    tiempoEstimado = "Menos de 1 minuto de lectura";
+    tiempoEstimado = "Menos de 1 minuto de lectura"
   } else if (tiempo == 1) {
-    tiempoEstimado = "Aproximadamente 1 minuto de lectura";
+    tiempoEstimado = "Aproximadamente 1 minuto de lectura"
   } else {
-    tiempoEstimado = "Aproximadamente " + tiempo + " minutos de lectura";
+    tiempoEstimado = "Aproximadamente " + tiempo + " minutos de lectura"
   }
-  txtTiempos[index].innerText = tiempoEstimado;
-  parrafos = 0;
+  txtTiempos[index].innerText = tiempoEstimado
+  parrafos = 0
 }
 
 function getHTML(url, callback) {
-  var req = new XMLHttpRequest();
+  var req = new XMLHttpRequest()
 
   req.onload = function () {
     if (callback && typeof callback === "function") {
-      callback(this.responseXML);
+      callback(this.responseXML)
     }
-  };
+  }
 
-  req.open("GET", url);
-  req.responseType = "document";
-  req.send();
+  req.open("GET", url)
+  req.responseType = "document"
+  req.send()
 }
 
 //Splash screen
 var index = 0,
-  text = "InfoTIC \n por Gonzalo Díaz de Vivar";
+  text = "InfoTIC \n por Gonzalo Díaz de Vivar"
 
 function Write() {
   if (index < text.length) {
     if (text.charAt(index) == "\n") {
-      document.getElementById("title-splash").innerHTML += "<br>";
+      document.getElementById("title-splash").innerHTML += "<br>"
     } else {
-      document.getElementById("title-splash").innerHTML += text.charAt(index);
+      document.getElementById("title-splash").innerHTML += text.charAt(index)
     }
-    index++;
-    setTimeout(Write, 50);
+    index++
+    setTimeout(Write, 50)
   } else {
     setTimeout(() => {
-      document.getElementById("splash-screen").classList.add("finish");
-      var cartas = document.getElementsByClassName("card");
+      document.getElementById("splash-screen").classList.add("finish")
+      var cartas = document.getElementsByClassName("card")
       for (i = 0; i < cartas.length; i++) {
-        cartas[i].classList.add("slide-effect");
+        cartas[i].classList.add("slide-effect")
       }
-      document
-        .getElementsByClassName("info-personal")[0]
-        .classList.add("subir");
-    }, 500);
+      document.getElementsByClassName("info-personal")[0].classList.add("subir")
+    }, 500)
   }
 }
-Write();
+Write()
