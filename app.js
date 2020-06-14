8f5862533c4226b599a43872f13d80b1eba7902c
GonzaDDV <53922815+GonzaDDV@users.noreply.github.com>
2020-06-14 14:21:14 -0300

    Entrega materialize

--- app.js
+++ app.js
@@ -1,123 +1,95 @@
-//Splash screen
-var index = 0,
-  text = "InfoTIC \n por Gonzalo Díaz de Vivar"
+const urls = ["coronavirus.html", "esi.html", "dengue.html", "cambios.html"];
+const txtTiempos = $(".tiempo-estimado");
+let parrafos = 0;
 
-function Write() {
-  if (index < text.length) {
-    if (text.charAt(index) == "\n") {
-      $("#title-splash").append("<br>")
-    } else {
-      $("#title-splash").append(text.charAt(index))
-    }
-    index++
-    setTimeout(Write, 50)
-  } else {
-    setTimeout(() => {
-      $("#splash-screen").addClass("finish")
-      let cartas = $(".card")
-      cartas.addClass("slide-effect")
-      $(".info-personal").addClass("subir")
-    }, 500)
-  }
-}
-Write()
-
-// Leer datos de notas desde JSON - Agregar el body de las notas para lograr SPA
-const cardTitles = $(".title-card")
-const cardDescriptions = $(".description-card")
-const containerNotas = $(".notas")
-const containerTitulares = $(".titulares")
-const notaCards = $(".card")
-
-let jsonFile
-
-$(document).ready(() => {
-  // Para conseguir la fecha del día de hoy
+$("document").ready(() => {
   var fecha = new Date(),
     dia = fecha.getDate(),
     meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     mes = fecha.getMonth(),
     año = fecha.getFullYear(),
-    txtFecha = $(".fecha-txt")[0]
+    txtFecha = document.getElementsByClassName("fecha-txt")[0];
 
-  txtFecha.textContent = "Noticias del día " + dia + "/" + meses[mes] + "/" + año
+  txtFecha.textContent =
+    "Noticias del día " + dia + "/" + meses[mes] + "/" + año;
+  getHTML(urls[0], function (response) {
+    parrafos = response.documentElement.getElementsByClassName("txt-principal");
+    funcionTotal(urls[0], 0);
+  });
+  getHTML(urls[1], function (response) {
+    parrafos = response.documentElement.getElementsByClassName("txt-principal");
+    funcionTotal(urls[1], 1);
+  });
+  getHTML(urls[2], function (response) {
+    parrafos = response.documentElement.getElementsByClassName("txt-principal");
+    funcionTotal(urls[2], 2);
+  });
+  getHTML(urls[3], function (response) {
+    parrafos = response.documentElement.getElementsByClassName("txt-principal");
+    funcionTotal(urls[3], 3);
+  });
+});
 
-  // Fetch(ear?) el JSON
-  $.getJSON("./notas.json", function (json) {
-    // Asignarlo a la variable
-    jsonFile = json
-    // Cambiar el titulo y descripcion de las cartas de las notas en base al JSON
-    cardTitles.each(function (i) {
-      $(this).text(json["Notas"][i].Titulo)
-    })
-    cardDescriptions.each(function (i) {
-      $(this).text(json["Notas"][i].Descripcion)
-    })
-  })
-})
+// Tiempo estimado de lectura
+function funcionTotal(url, index) {
+  const palabrasPorMinuto = 200;
+  var tiempoEstimado = 0;
+  var palabrasTotales = 0;
+  for (i = 0; i < parrafos.length; i++) {
+    palabrasTotales += parrafos[i].textContent.trim().split(/\s+/).length;
+  }
 
-notaCards.each(function () {
-  $(this).click(function () {
-    containerNotas.show()
-    containerTitulares.hide()
-    containerNotas.html("")
-    containerNotas.append(
-      "<div class='fixed-action-btn'><a class='btn-floating btn-large blue'><i class='material-icons'>home</i></a></div>"
-    )
-    $(".progress").css("display", "block")
-
-    $(".fixed-action-btn").click(function () {
-      volverAHome()
-    })
-
-    // No funciona la animacion :(
-    $("html, body").animate({ scrollTop: 0 }, "slow")
-
-    jsonFile["Notas"].forEach((nota, i) => {
-      if ($(this).attr("id") === nota.Key) {
-        nota.Parrafos.map((parrafoActual) => {
-          containerNotas.append(
-            `<${parrafoActual.Etiqueta} ${parrafoActual.Propiedades ? parrafoActual.Propiedades : ""} class="${
-              parrafoActual.Clases
-            }"> ${parrafoActual.Texto ? parrafoActual.Texto : ""} </${parrafoActual.Etiqueta}>`
-          )
-        })
-
-        let tituloNota = $(".titulo-nota")
-        tituloNota.html(nota.Titulo)
-      }
-    })
-
-    // Animar en scroll
-    const fadeElements = $(".fade-scroll")
-    const optionsAppear = {
-      threshold: 0.8,
-    }
-    const scrollObserver = new IntersectionObserver((entries, appearOnScroll) => {
-      entries.forEach((entry) => {
-        if (!entry.isIntersecting) return
-        else {
-          entry.target.classList.add("fade-in")
-          appearOnScroll.unobserve(entry.target)
-        }
-      })
-    }, optionsAppear)
-
-    for (let el of fadeElements) {
-      scrollObserver.observe(el)
-    }
-  })
-})
-
-const indicadorScroll = $(".determinate")
-$(window).scroll(function () {
-  let altura = document.documentElement.scrollHeight - document.documentElement.clientHeight
-  let porcentaje = (window.scrollY / altura) * 100
-  indicadorScroll.width(porcentaje + "%")
-})
-
-const volverAHome = () => {
-  containerNotas.hide()
-  containerTitulares.show()
-  $(".progress").css("display", "none")
+  var tiempoDecimal = palabrasTotales / palabrasPorMinuto;
+  var tiempo = Math.ceil(tiempoDecimal);
+  if (tiempo == 0) {
+    tiempoEstimado = "Menos de 1 minuto de lectura";
+  } else if (tiempo == 1) {
+    tiempoEstimado = "Aproximadamente 1 minuto de lectura";
+  } else {
+    tiempoEstimado = "Aproximadamente " + tiempo + " minutos de lectura";
+  }
+  txtTiempos[index].innerText = tiempoEstimado;
+  parrafos = 0;
 }
+
+function getHTML(url, callback) {
+  var req = new XMLHttpRequest();
+
+  req.onload = function () {
+    if (callback && typeof callback === "function") {
+      callback(this.responseXML);
+    }
+  };
+
+  req.open("GET", url);
+  req.responseType = "document";
+  req.send();
+}
+
+//Splash screen
+var index = 0,
+  text = "InfoTIC \n por Gonzalo Díaz de Vivar";
+
+function Write() {
+  if (index < text.length) {
+    if (text.charAt(index) == "\n") {
+      document.getElementById("title-splash").innerHTML += "<br>";
+    } else {
+      document.getElementById("title-splash").innerHTML += text.charAt(index);
+    }
+    index++;
+    setTimeout(Write, 50);
+  } else {
+    setTimeout(() => {
+      document.getElementById("splash-screen").classList.add("finish");
+      var cartas = document.getElementsByClassName("card");
+      for (i = 0; i < cartas.length; i++) {
+        cartas[i].classList.add("slide-effect");
+      }
+      document
+        .getElementsByClassName("info-personal")[0]
+        .classList.add("subir");
+    }, 500);
+  }
+}
+Write();
--- appNota.js
+++ appNota.js
@@ -1,26 +1,9 @@
 // Indicador Scroll
-let indicador_scroll = $(".determinate")
+let indicador_scroll = document.getElementsByClassName("determinate")[0];
 window.onscroll = (e) => {
   let altura =
     document.documentElement.scrollHeight -
-    document.documentElement.clientHeight
-  let porcentaje = (window.scrollY / altura) * 100
-  indicador_scroll.width(porcentaje + "%")
-}
-
-// Get título de la nota
-const tituloPagina = $(".titulo-nota")
-
-var url = location.href
-var urlFilename = url.substring(url.lastIndexOf("/") + 1)
-
-$(document).ready(() => {
-  $.getJSON("../notas.json", function (json) {
-    for (let nota of json["Notas"]) {
-      if (nota.Key + ".html" === urlFilename) {
-        $(".titulo-nota").text(nota.Titulo)
-        $(document).prop("title", `${nota.Titulo} - InfoTIC`)
-      }
-    }
-  })
-})
+    document.documentElement.clientHeight;
+  let porcentaje = (window.scrollY / altura) * 100;
+  indicador_scroll.style.width = porcentaje + "%";
+};
